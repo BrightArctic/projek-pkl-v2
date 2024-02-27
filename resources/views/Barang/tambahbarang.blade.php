a83567925f93d19b217f7fedba16884ed85bf9a0
BrightArctic <alghazaliramzisalim2@gmail.com>
2024-02-27 15:13:02 +0700

    initiallize

--- resources/views/Barang/tambahbarang.blade.php
+++ resources/views/Barang/tambahbarang.blade.php
@@ -156,25 +156,56 @@
     Webcam.attach('#camera');
 
     function previewFile(file) {
-        var preview = document.getElementById('results');
-        var reader = new FileReader();
+    var preview = document.getElementById('results');
+    var reader = new FileReader();
 
-        reader.onloadend = function() {
-            preview.innerHTML = '<img src="' + reader.result + '" class="img-fluid mt-4"/>';
+    reader.onloadend = function() {
+        var img = new Image();
+        img.src = reader.result;
+
+        img.onload = function() {
+            var canvas = document.createElement('canvas');
+            var ctx = canvas.getContext('2d');
+
+            var MAX_WIDTH = 350;
+            var MAX_HEIGHT = 250;
+            var width = img.width;
+            var height = img.height;
+
+            if (width > height) {
+                if (width > MAX_WIDTH) {
+                    height *= MAX_WIDTH / width;
+                    width = MAX_WIDTH;
+                }
+            } else {
+                if (height > MAX_HEIGHT) {
+                    width *= MAX_HEIGHT / height;
+                    height = MAX_HEIGHT;
+                }
+            }
+
+            canvas.width = width;
+            canvas.height = height;
+            ctx.drawImage(img, 0, 0, width, height);
+
+            var dataURL = canvas.toDataURL('image/jpeg');
+            preview.innerHTML = '<img src="' + dataURL + '" class="img-fluid mt-4"/>';
         };
+    };
 
-        if (file) {
-            reader.readAsDataURL(file);
-            // Hide the text
-            document.getElementById('fileInputWrapper').getElementsByTagName('label')[0].style.display = 'none';
-            // Hide the camera
-            document.getElementById('camera').style.display = 'none';
-            // Hide the "Take Snapshot" button
-            document.getElementById('snapshotBtn').style.display = 'none';
-        } else {
-            preview.innerHTML = 'Your captured image will appear here...';
-        }
+    if (file) {
+        reader.readAsDataURL(file);
+        // Hide the text
+        document.getElementById('fileInputWrapper').getElementsByTagName('label')[0].style.display = 'none';
+        // Hide the camera
+        document.getElementById('camera').style.display = 'none';
+        // Hide the "Take Snapshot" button
+        document.getElementById('snapshotBtn').style.display = 'none';
+    } else {
+        preview.innerHTML = 'Your captured image will appear here...';
     }
+}
+
 
     function handleFileInput() {
         var file = document.getElementById('fileInput').files[0];
